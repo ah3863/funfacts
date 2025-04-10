f3c4194844c8b6bfb782c6dcac387405677a6a2f
ah3863 <ah3863@nyu.edu>
2025-04-10 12:09:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
 
-    // Reveal the answer when the button is clicked
-    $(".reveal-answer").click(function() {
-      $(this).next(".answer").slideDown();
-      $(this).prop("disabled", true); // Disable button after it's clicked
-    });
+  // Hide all answers initially
+  $(".answer").hide();
+
+  // Reveal the answer when the button is clicked
+  $(".reveal-answer").click(function() {
+    $(this).next(".answer").slideDown();
+    $(this).text("Answer Shown").prop("disabled", true); // Optional text change
   });
+
+});
